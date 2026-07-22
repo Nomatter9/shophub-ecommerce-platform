6964cc34f97b1bc0b16ce80ad10075610447c977
Nomatter9 <mnomatter9@gmail.com>
2026-07-22 22:38:47 +0200

    fix vite env types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_STATIC_FILE_URL: string;
-  // add more env vars here...
+  readonly VITE_STRIPE_PUBLISHABLE_KEY: string;
 }
 
 interface ImportMeta {
